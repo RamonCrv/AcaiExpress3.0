2d47a9b35931732065968a45bd69618596e78cee
SuperRatonildo <ramonpinhoacademico@gmail.com>
2019-09-11 01:45:27 -0300

    GG %6 activity cadastro add

--- app/src/main/java/com/example/gs/acaiexpress/Cadastro.java
+++ app/src/main/java/com/example/gs/acaiexpress/Cadastro.java
@@ -1,5 +1,7 @@
 package com.example.gs.acaiexpress;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,15 +10,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.example.gs.acaiexpress.ui.main.Dados;
+import com.example.gs.acaiexpress.ui.main.MainFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.example.gs.acaiexpress.ui.main.MainFragment;
-public class MainActivity extends AppCompatActivity  {
+
+public class Cadastro extends AppCompatActivity {
+
     private Button btnRegistrar;
-    private Button btnLogar;
+
     private EditText editEmail;
     private EditText editSenha;
     private FirebaseAuth mAuth;
@@ -26,7 +31,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_cadastro);
         inicializarComponentes();
         eventoClicks();
         mAuth = FirebaseAuth.getInstance();
@@ -39,52 +44,49 @@ public class MainActivity extends AppCompatActivity  {
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, Cadastro.class);
-                startActivity(i);
-            }
-        });
-        btnLogar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 String email = editEmail.getText().toString().trim();
                 String senha = editSenha.getText().toString().trim();
-                login(email, senha);
+                criarUser(email,senha);
             }
         });
+
     }
-
-    //FAZ O LOGIN TRADICIONAL
-    private void login(String email, String senha) {
-        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)) {
-            alert("prencha todos os campos");
-        } else {
-            mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-
-                @Override
-                public void onComplete(Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        Intent i = new Intent(MainActivity.this, Dados.class);
-                        startActivity(i);
-                        //alert("Logou");
-                    } else {
-                        alert("Email ou senha incorreto");
+    //CRIAR USUARIO TRADICIONAL
+    private void criarUser(String email, String senha){
+        if(email.contains("@") == false){
+            alert("ENTRE COM UM EMAIL VALIDO!!!");
+        }else{
+            if (senha.length() <=7 ){
+                alert("SENHA MUITO PEQUENA");
+            }else{
+                mAuth.createUserWithEmailAndPassword(email,senha).addOnCompleteListener(Cadastro.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete( Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            mAuth.getCurrentUser();
+                            alert("Cadastrado com Sucesso!");
+                            Intent i =new Intent(Cadastro.this, MainActivity.class);
+                            startActivity(i);
+                        }else{
+                            alert("Email já Cadastrado");
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     }
+
     //INICIALIZA COMPONENTES
     private void inicializarComponentes() {
         btnRegistrar = (Button) findViewById(R.id.btCadastrar);
-        btnLogar = (Button) findViewById(R.id.btLogar);
         editEmail = (EditText) findViewById(R.id.editEmail);
         editSenha = (EditText) findViewById(R.id.editSenha);
     }
     //MOSTRA MSG
     private  void alert (String msg){
-        final  Toast toast = new Toast(getApplicationContext());
+        final Toast toast = new Toast(getApplicationContext());
         toast.setGravity(Gravity.CENTER_VERTICAL, 0,0);
         toast.setDuration(Toast.LENGTH_SHORT);
-        Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+        Toast.makeText(Cadastro.this,msg,Toast.LENGTH_SHORT).show();
     }
 }
