776416e2e120393be689fa890abbd118c2f47d05
Ramon <ratonildoramon11@gmail.com>
2019-10-10 03:05:09 -0300

    Sem bug da imagem n aparecer e não 0 mais a posição

---
+++ app/src/main/java/com/example/gs/acaiexpress/ReseteSenha.java
@@ -0,0 +1,14 @@
+package com.example.gs.acaiexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReseteSenha extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resete_senha);
+    }
+}
